dcae4632855d8c93fde7ba25edae3cae47048d15
Alireza Shateri <alirezashateri7@gmail.com>
2025-02-22 15:27:23 -0500

    :bug: fix: fix invalid resource handle for streams on different gpu devices

--- src/main.cu
+++ src/main.cu
@@ -1,8 +1,9 @@
+#include <chrono>
 #include <cstring>
 #include <cuda_runtime.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 
 #define DATA_SIZE 1024 * 1024 * 256
 #define SRC_GPU 0
@@ -18,13 +19,24 @@
     }                                                                          \
   }
 
+long openmp_sum(int *data, size_t size) {
+  long sum = 0;
+  int num_threads = omp_get_max_threads();
+#pragma omp parallel for simd reduction(+ : sum) num_threads(num_threads)
+  for (size_t i = 0; i < size; i++) {
+    sum += data[i];
+  }
+
+  return sum;
+}
+
 // Transfer data host-to-device or device-to-host async
 void transfer_data(int gpu_id, int *src_data, int *host_buffer,
                    size_t data_size, cudaStream_t stream,
                    cudaEvent_t **timing_events, bool dtoh = true) {
   CHECK_CUDA(cudaSetDevice(gpu_id));
 
-  cudaEvent_t * events = (cudaEvent_t *)malloc(2 * sizeof(cudaEvent_t));
+  cudaEvent_t *events = (cudaEvent_t *)malloc(2 * sizeof(cudaEvent_t));
 
   CHECK_CUDA(cudaEventCreate(&events[0]));
   CHECK_CUDA(cudaEventCreate(&events[1]));
@@ -74,14 +86,24 @@ int main(int argc, char **argv) {
 
   cudaEvent_t *timing_events_src_host;
   printf("Data generated on src GPU, sending from src to host\n");
-  transfer_data(SRC_GPU, gpu_data, data, DATA_SIZE * sizeof(int), src_stream, &timing_events_src_host);
+  transfer_data(SRC_GPU, gpu_data, data, DATA_SIZE * sizeof(int), src_stream,
+                &timing_events_src_host);
+
+
+  auto start = std::chrono::high_resolution_clock::now();
+  long result = openmp_sum(data, DATA_SIZE);
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+  printf("Timing for OpenMP sum: %ld\n", duration.count());
 
   CHECK_CUDA(cudaSetDevice(DEST_GPU));
   CHECK_CUDA(cudaStreamCreate(&dest_stream));
 
   cudaEvent_t *timing_events_host_dest;
   printf("Data received on host, sending from host to dest\n");
-  transfer_data(DEST_GPU, gpu_data, data, DATA_SIZE * sizeof(int), dest_stream, &timing_events_host_dest, false);
+  transfer_data(DEST_GPU, gpu_data, &result, sizeof(long), dest_stream,
+                &timing_events_host_dest, false);
 
   CHECK_CUDA(cudaSetDevice(SRC_GPU));
   CHECK_CUDA(cudaStreamSynchronize(src_stream));
@@ -90,12 +112,13 @@ int main(int argc, char **argv) {
 
   float src_host_timing, host_dest_timing;
 
-  CHECK_CUDA(cudaEventElapsedTime(&src_host_timing, timing_events_src_host[0], timing_events_src_host[1]));
-  CHECK_CUDA(cudaEventElapsedTime(&host_dest_timing, timing_events_host_dest[0], timing_events_host_dest[1]));
-
+  CHECK_CUDA(cudaEventElapsedTime(&src_host_timing, timing_events_src_host[0],
+                                  timing_events_src_host[1]));
+  CHECK_CUDA(cudaEventElapsedTime(&host_dest_timing, timing_events_host_dest[0],
+                                  timing_events_host_dest[1]));
 
   printf("Src to Host: %f\n", src_host_timing);
   printf("Host to Dest: %f\n", host_dest_timing);
 
-  return 0; 
+  return 0;
 }
