2f6e016483f81d35d9b06cd8cdd07b12f30350be
Alireza Shateri <alirezashateri7@gmail.com>
2025-02-22 16:24:43 -0500

    :wrench: chore: add run functions for both scenarios

--- src/main.cu
+++ src/main.cu
@@ -69,40 +69,24 @@ void generate_data(int gpu_id, int *host_buffer, int *gpu_buffer,
                 &timing_events, false);
 }
 
-int main(int argc, char **argv) {
-
-  CHECK_CUDA(cudaSetDevice(SRC_GPU));
-  int *data = (int *)malloc(DATA_SIZE * sizeof(int));
-  int *gpu_data;
-  CHECK_CUDA(cudaMalloc((void **)&gpu_data, DATA_SIZE * sizeof(int)));
+void compute_on_destination(int src_gpu, int dest_gpu, int *host_buffer,
+                            int *src_data, int *dest_data) {
+  memset(host_buffer, 0, DATA_SIZE * sizeof(int));
 
   cudaStream_t src_stream, dest_stream;
 
   CHECK_CUDA(cudaStreamCreate(&src_stream));
-
-  generate_data(0, data, gpu_data, DATA_SIZE * sizeof(int), src_stream);
-
-  memset(data, 0, DATA_SIZE * sizeof(int));
-
   cudaEvent_t *timing_events_src_host;
   printf("Data generated on src GPU, sending from src to host\n");
-  transfer_data(SRC_GPU, gpu_data, data, DATA_SIZE * sizeof(int), src_stream,
-                &timing_events_src_host);
-
-
-  auto start = std::chrono::high_resolution_clock::now();
-  int result = openmp_sum(data, DATA_SIZE);
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-
-  printf("Timing for OpenMP sum: %ld\n", duration.count());
+  transfer_data(SRC_GPU, src_data, host_buffer, DATA_SIZE * sizeof(int),
+                src_stream, &timing_events_src_host);
 
   CHECK_CUDA(cudaSetDevice(DEST_GPU));
   CHECK_CUDA(cudaStreamCreate(&dest_stream));
 
   cudaEvent_t *timing_events_host_dest;
   printf("Data received on host, sending from host to dest\n");
-  transfer_data(DEST_GPU, gpu_data, &result, sizeof(int), dest_stream,
+  transfer_data(DEST_GPU, dest_data, host_buffer, DATA_SIZE * sizeof(int), dest_stream,
                 &timing_events_host_dest, false);
 
   CHECK_CUDA(cudaSetDevice(SRC_GPU));
@@ -119,6 +103,66 @@ int main(int argc, char **argv) {
 
   printf("Src to Host: %f\n", src_host_timing);
   printf("Host to Dest: %f\n", host_dest_timing);
+}
+
+void compute_on_path(int src_gpu, int dest_gpu, int *host_buffer, int *src_data, int *dest_data) {
+  cudaStream_t src_stream, dest_stream;
+
+  CHECK_CUDA(cudaStreamCreate(&src_stream));
+
+  memset(host_buffer, 0, DATA_SIZE * sizeof(int));
+
+  cudaEvent_t *timing_events_src_host;
+  printf("Data generated on src GPU, sending from src to host\n");
+  transfer_data(SRC_GPU, src_data, host_buffer, DATA_SIZE * sizeof(int), src_stream,
+                &timing_events_src_host);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  int result = openmp_sum(host_buffer, DATA_SIZE);
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+  printf("Timing for OpenMP sum: %ld\n", duration.count());
+
+  CHECK_CUDA(cudaSetDevice(DEST_GPU));
+  CHECK_CUDA(cudaStreamCreate(&dest_stream));
+
+  cudaEvent_t *timing_events_host_dest;
+  printf("Data received on host, sending from host to dest\n");
+  transfer_data(DEST_GPU, dest_data, &result, sizeof(int), dest_stream,
+                &timing_events_host_dest, false);
+
+  CHECK_CUDA(cudaSetDevice(SRC_GPU));
+  CHECK_CUDA(cudaStreamSynchronize(src_stream));
+  CHECK_CUDA(cudaSetDevice(DEST_GPU));
+  CHECK_CUDA(cudaStreamSynchronize(dest_stream));
+
+  float src_host_timing, host_dest_timing;
+
+  CHECK_CUDA(cudaEventElapsedTime(&src_host_timing, timing_events_src_host[0],
+                                  timing_events_src_host[1]));
+  CHECK_CUDA(cudaEventElapsedTime(&host_dest_timing, timing_events_host_dest[0],
+                                  timing_events_host_dest[1]));
+
+  printf("Src to Host: %f\n", src_host_timing);
+  printf("Host to Dest: %f\n", host_dest_timing);
+}
+
+int main(int argc, char **argv) {
+  CHECK_CUDA(cudaSetDevice(SRC_GPU));
+  int *host_buffer = (int *)malloc(DATA_SIZE * sizeof(int));
+  int *src_gpu_data;
+  CHECK_CUDA(cudaMalloc((void **)&src_gpu_data, DATA_SIZE * sizeof(int)));
+
+  CHECK_CUDA(cudaSetDevice(DEST_GPU));
+  int *dest_gpu_data;
+  CHECK_CUDA(cudaMalloc((void **)&dest_gpu_data, DATA_SIZE * sizeof(int)));
+
+  generate_data(0, host_buffer, src_gpu_data, DATA_SIZE * sizeof(int), 0);
+
+  compute_on_destination(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data, dest_gpu_data);
+  compute_on_path(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data, dest_gpu_data);
 
   return 0;
 }
