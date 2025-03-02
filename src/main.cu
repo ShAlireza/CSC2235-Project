4621cb9308e451e664090a9026f54a31a5ffe532
Alireza Shateri <alirezashateri7@gmail.com>
2025-03-02 17:15:59 -0500

    :hammer: chore: add prints

--- src/main.cu
+++ src/main.cu
@@ -411,8 +411,9 @@ int main(int argc, char **argv) {
 
   // Run the two scenarios
 
-  // compute_on_destination(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
-  //                        dest_gpu_data);
+  printf("Starting compute on destination withouth any threading\n");
+  compute_on_destination(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
+                         dest_gpu_data);
   printf("Starting compute on destination with 1 thread\n");
   compute_on_destination_pipelined(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
                                    dest_gpu_data, 1);
@@ -423,6 +424,14 @@ int main(int argc, char **argv) {
   printf("Starting compute on destination with 8 thread\n");
   compute_on_destination_pipelined(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
                                    dest_gpu_data, 8);
+
+  printf("Starting compute on destination with 16 thread\n");
+  compute_on_destination_pipelined(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
+                                   dest_gpu_data, 16);
+
+  printf("Starting compute on destination with 32 thread\n");
+  compute_on_destination_pipelined(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
+                                   dest_gpu_data, 32);
   // compute_on_path(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
   // dest_gpu_data);
 
