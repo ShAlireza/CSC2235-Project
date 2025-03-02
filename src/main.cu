f674b4627c163ebbde92e285fd6647030f873aee
Alireza Shateri <alirezashateri7@gmail.com>
2025-03-02 17:58:16 -0500

    :rocket: feat: add on-path scenario

--- src/main.cu
+++ src/main.cu
@@ -341,12 +341,12 @@ void compute_on_path_thread(int src_gpu, int dest_gpu, int *host_buffer,
 
   // Do the sum reduction on the intermediate host using openmp
   auto start = std::chrono::high_resolution_clock::now();
-  int result = openmp_sum(host_buffer, ITEMS_COUNT);
+  int result = openmp_sum(&host_buffer[start_index], chunk_size / sizeof(int));
   auto end = std::chrono::high_resolution_clock::now();
   auto duration =
       std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-  printf("Timing for OpenMP sum: %ld\n", duration.count());
+  // printf("Timing for OpenMP sum: %ld\n", duration.count());
 
   CHECK_CUDA(cudaSetDevice(DEST_GPU));
   CHECK_CUDA(cudaEventCreate(&second_copy_events[0]));
