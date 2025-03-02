e94713367d057999cb80f4d09be1b2b73fa407ca
Alireza Shateri <alirezashateri7@gmail.com>
2025-03-02 17:13:57 -0500

    :hammer: chore: add prints

--- src/main.cu
+++ src/main.cu
@@ -411,10 +411,18 @@ int main(int argc, char **argv) {
 
   // Run the two scenarios
 
-  compute_on_destination(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
-                         dest_gpu_data);
+  // compute_on_destination(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
+  //                        dest_gpu_data);
+  printf("Starting compute on destination with 1 thread\n");
+  compute_on_destination_pipelined(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
+                                   dest_gpu_data, 1);
+  printf("Starting compute on destination with 4 thread\n");
   compute_on_destination_pipelined(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
                                    dest_gpu_data, 4);
+
+  printf("Starting compute on destination with 8 thread\n");
+  compute_on_destination_pipelined(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
+                                   dest_gpu_data, 8);
   // compute_on_path(SRC_GPU, DEST_GPU, host_buffer, src_gpu_data,
   // dest_gpu_data);
 
