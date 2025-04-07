a69978348cfa8d817ccd8f8c8431eaf226fe7203
tabbahch <christiantabbah@gmail.com>
2025-04-07 17:25:36 -0400

    added

--- src/deduplicate.cu
+++ src/deduplicate.cu
@@ -3,6 +3,62 @@
 #include <iostream>
 #include <thread>
 
+/*
+#pragma once
+
+#include <atomic>
+#include <map>
+#include <mutex>
+#include <vector>
+
+#define DISTINCT_MERGE_BUFFER_SIZE                                             \
+  1024 * 1024 * 256 // WARN: we should use smalle send buffer size
+#define DISTINCT_MERGE_BUFFER_THRESHOLD 1024 * 256
+
+struct DistinctMerge {
+private:
+  std::map<int, bool> seen_values{};
+  int send_buffer[DISTINCT_MERGE_BUFFER_SIZE]{-1};
+  int send_buffer_start_index{0};
+  int send_buffer_end_index{0};
+
+  std::mutex send_buffer_mutex{};
+  std::mutex seen_values_mutex{};
+
+  std::vector<int *> receive_buffers{};
+  std::vector<int> receive_buffer_sizes{};
+
+public:
+  DistinctMerge() = default;
+  DistinctMerge(const std::vector<int *> &receive_buffers,
+                const std::vector<int> &receive_buffer_sizes);
+
+  int check_value(int value);
+
+  bool stage(int value);
+
+  void sender();
+};
+
+struct DistinctMergeGPU {
+public:
+  int gpu_id{0};
+  int tuples_count{0};
+  int chunk_size{0};
+  int *gpu_data{nullptr};
+  int *destination_buffer{nullptr};
+
+  DistinctMergeGPU(int gpu_id, int tuples_count, int chunk_size);
+
+  DistinctMerge *cpu_merger;
+
+  void exec(int start_index);
+
+  void start();
+};
+
+*/
+
 #define DEDUPLICATION_TUPLES_COUNT 1024 * 1024 * 128
 #define DEDUPLICATION_CHUNK_SIZE 1024 * 1024
 
@@ -23,9 +79,13 @@ int main(int argc, char *argv[]) {
                                DEDUPLICATION_CHUNK_SIZE);
 
   std::cout << "Creating CPU merger" << std::endl;
-  // DistinctMerge merger(
-  //     {merger_gpu1.destination_buffer, merger_gpu2.destination_buffer},
-  //     {DEDUPLICATION_TUPLES_COUNT, DEDUPLICATION_TUPLES_COUNT});
+
+  std::vector<int *> recv_buffers = {
+      merger_gpu1.destination_buffer, merger_gpu2.destination_buffer};
+  std::vector<int> recv_buffer_sizes = { 
+      DEDUPLICATION_TUPLES_COUNT, DEDUPLICATION_TUPLES_COUNT};
+
+  DistinctMerge merger(recv_buffers, recv_buffer_sizes);
 
   // merger_gpu1.cpu_merger = &merger;
   // merger_gpu2.cpu_merger = &merger;
