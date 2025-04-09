0fab4c7cb982b92b59d7c718b8d98f9fe0d59176
Alireza Shateri <alirezashateri7@gmail.com>
2025-04-09 17:48:37 -0400

    :hammer: chore: add an infinite loop to prevent program from exiting

--- src/deduplicate.cu
+++ src/deduplicate.cu
@@ -34,13 +34,15 @@ int main(int argc, char *argv[]) {
 
   DistinctMerge merger(recv_buffers, recv_buffer_sizes);
 
-  // merger_gpu1.cpu_merger = &merger;
-  // merger_gpu2.cpu_merger = &merger;
+  merger_gpu1.cpu_merger = &merger;
+  merger_gpu2.cpu_merger = &merger;
 
   std::cout << "Starting GPU 1 merger" << std::endl;
   // std::thread t1(start_deduplication, std::ref(merger_gpu1));
   std::cout << "Starting GPU 2 merger" << std::endl;
   // std::thread t2(start_deduplication, std::ref(merger_gpu2));
+
+  while(true);
 
   return 0;
 }
