863dc334bec0b0307912dcf066daea7d137219e6
Alireza Shateri <alirezashateri7@gmail.com>
2025-04-14 23:33:27 -0400

    :bug: fix: fix syntax error in distinct merger

--- src/distinct_merge.h
+++ src/distinct_merge.h
@@ -8,8 +8,8 @@
 
 #define DISTINCT_MERGE_BUFFER_SIZE                                             \
   1024 * 1024 * 256 // WARN: we should use smalle send buffer size
-#define DISTINCT_MERGE_BUFFER_THRESHOLD 16
-#define DISTINCT_MERGE_SEND_CHUNK_SIZE 8
+#define DISTINCT_MERGE_BUFFER_THRESHOLD 1024 * 256
+#define DISTINCT_MERGE_SEND_CHUNK_SIZE 1024 * 1024
 
 struct DistinctMerge {
 private:
