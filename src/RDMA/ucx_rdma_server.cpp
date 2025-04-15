5c9069d647179a475be7b1d8650b2c2342fb8bb1
Alireza Shateri <alirezashateri7@gmail.com>
2025-04-15 14:25:29 -0400

    :bug: fix: fix syntax error in distinct merger

--- src/RDMA/ucx_rdma_server.cpp
+++ src/RDMA/ucx_rdma_server.cpp
@@ -174,7 +174,7 @@ void receiver_thread(int *buffer, DistinctMergeDest *merger) {
         // printf("\n");
         //
         while (buffer[1 + old_counter] != 0) {
-          printf("%d ", 1000 + buffer[1 + old_counter]);
+          printf("%d ", 1000 + buffer[1 + old_counter++]);
           // int check_value = merger->check_value(buffer[1 + old_counter++]);
           // if (check_value != -1) {
           //   merger->stage(check_value);
