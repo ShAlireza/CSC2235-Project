22357d0a689dd9827f9590a1d34eadbc239aab0f
tabbahch <christiantabbah@gmail.com>
2025-04-16 22:06:06 -0400

    :whale: added randomness to generate_data

--- src/utils.h
+++ src/utils.h
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <thread>
+#include <random>
 
 #define ITEMS_COUNT 1024 * 1024 * 256 * 4 / 2
 
@@ -36,12 +37,28 @@
 // }
 
 void generate_data(int gpu_id, int *gpu_buffer, size_t tuples_count,
-                   int offset = 0) {
+                   int offset = 0, float randomness_factor = 1.0f) {
   // Generate random data on CPU
+  // simple boundary checks:
+  if (randomness_factor < 0.0f) randomness_factor = 0.0f;
+  if (randomness_factor > 1.0f) randomness_factor = 1.0f;
+
+  // WARN: number of potential unique values depends on tuples_count
+  size_t unique_values = std::max((size_t)(tuples_count * randomness_factor), (size_t)1);
+
+
   int *host_buffer = (int *)malloc(tuples_count * sizeof(int));
-  cudaEvent_t *timing_events;
+
+  std::default_random_engine rng(std::random_device{}());
+  std::uniform_int_distribution<int> dist(0, unique_values - 1);
+
+  int val = offset + dist(rng);
+
   for (int j = 0; j < tuples_count; j++) {
-    host_buffer[j] = offset + j;
+    if (randomness_factor == 0.0f) {
+      val = offset + j;
+    }
+    host_buffer[j] = val;
   }
     // Transfer data to GPU
   CHECK_CUDA(cudaSetDevice(gpu_id));
